0c60217ebcc860c0219eb9b27c5c7d2396a1f9b0
ravneet556 <kaurravneet556@gmail.com>
2020-05-05 20:31:46 -0400

    segues and unwind

--- SWOOSH-APP/ViewController.swift
+++ SWOOSH-APP/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
        /* swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
         backImg.frame = view.frame;*/
     }
-
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
 
 }
 
